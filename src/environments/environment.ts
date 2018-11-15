1aecb98b4e930a447b18c05308885a4f8d649d50
chiangs <stephen.e.chiang@gmail.com>
2018-11-15 19:44:28 +0100

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { firebaseKey } from '../keys';
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyBByNNEGz_si93_r92Xk9nCRDz5Ql9Rkuo',
-    authDomain: 'ngfitnesstracker-2026c.firebaseapp.com',
-    databaseURL: 'https://ngfitnesstracker-2026c.firebaseio.com',
-    projectId: 'ngfitnesstracker-2026c',
-    storageBucket: 'ngfitnesstracker-2026c.appspot.com',
-    messagingSenderId: '400697086575'
-  }
+  firebase: firebaseKey
 };
 
 /*
